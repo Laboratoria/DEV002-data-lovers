0b4a5df57f1b791838efee37ffe7f03c624edaae
Laura Buitrago <buitrago92943@gmail.com>
2022-11-15 10:47:37 -0500

    Actuazliacion html

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+let containerSidenav = document.createElement("div");
+containerSidenav.setAttribute("class", "container-sidenav");
+
 console.log(example, data);
