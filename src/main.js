c6cd37042205ba0d0a189fe652a9aabb54a5a1d5
Kamila Ojeda <beau.ojeda@gmail.com>
2022-11-15 14:41:44 -0300

    Organiza lineas de código con dupla. Quita líneas de código comentadas

--- src/main.js
+++ src/main.js
@@ -62,30 +62,3 @@ function listElements(elements) {
         list.appendChild(li);
     })
 }
-
-/*function reverseNames() {
-   let names = data.characters.map((character) => character.name).sort().reverse();
-   elemento.innerHTML = printElements(names);
-}*/
-/*let order = document.getElementById("filtroReverseOrder");
-order.addEventListener("click", () => {
-    list.innerHTML = '';
-    let names = data.characters.map((character) => character.name).sort();
-    listElements(names);
-});*/
-
-//console.log(reverseOrder);
-//console.log(data.characters.map((character) => character.name).sort().reverse());
-/* let list = [5, 3, 4, 0, 9];
-console.log(list.sort().reverse());
-console.log(list.map(function (a) {
-    return a + "a";
-}))
-
-function cuadrado(a) {
-    return a * a;
-}
-
-console.log(list.map(cuadrado));
-
-console.log(list.map((a) => a * a)); */
