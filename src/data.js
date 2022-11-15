54496bdeb9ed7bca2ba894efb66afc11239ef212
Kamila Ojeda <beau.ojeda@gmail.com>
2022-11-15 14:38:41 -0300

    Revisa funcion ordenar. Agrega ajuste de estilos para el selector card-element. Organiza lineas de código con dupla.

--- src/data.js
+++ src/data.js
@@ -1,13 +1,7 @@
-// estas funciones son de ejemplo
 
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
 
 export const printCharacters = (data) => {
   return data.characters
--- src/main.js
+++ src/main.js
@@ -1,46 +1,78 @@
-import { example, orderCharacters, printCharacters, printElements } from './data.js';
-// import data from './data/lol/lol.js';
+import { example, orderCharacters, printCharacters, printElements, showCharacterbyName } from './data.js';
 import data from './data/harrypotter/data.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
+
+
+
+
+
 
 let list = document.getElementById("characteresList");
-listElements(data.characters.map((character) => character.name));
-
-/*ordena caracteres a-z*/
 let orderAscendent = document.getElementById("filterAscendent");
+let orderDescendent = document.getElementById("filterDescendent");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+listElements(data.characters.map((character) => character.name + character.house));
 orderAscendent.addEventListener("click", () => {
     list.innerHTML = '';
     let names = data.characters.map((character) => character.name).sort();
     listElements(names);
 });
-/*ordena caracteres z-a*/
-let orderDescendent = document.getElementById("filterDescendent");
 orderDescendent.addEventListener("click", () => {
     list.innerHTML = '';
     let names = data.characters.map((character) => character.name).sort().reverse();
     listElements(names);
 });
 
-/*function reverseNames() {
-   let names = data.characters.map((character) => character.name).sort().reverse();
-   elemento.innerHTML = printElements(names);
-}*/
-
 function listElements(elements) {
     elements.forEach((cardElement) => {
         let li = document.createElement('li');
+        let liHouses = document.createElement
         li.innerHTML = cardElement;
         li.className = 'card-element';
         list.appendChild(li);
     })
 }
+
+/*function reverseNames() {
+   let names = data.characters.map((character) => character.name).sort().reverse();
+   elemento.innerHTML = printElements(names);
+}*/
 /*let order = document.getElementById("filtroReverseOrder");
 order.addEventListener("click", () => {
     list.innerHTML = '';
     let names = data.characters.map((character) => character.name).sort();
     listElements(names);
 });*/
-
 
 //console.log(reverseOrder);
 //console.log(data.characters.map((character) => character.name).sort().reverse());
