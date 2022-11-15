1a623dbdb7d84bc644c8c127c56330beb5c2d506
beresdev <beresdev@gmail.com>
2022-11-15 11:36:19 -0600

    Revisión de funcion buscador. Ajuste de estilos para la sección de las cards. Organización de las lineas de código para reducir conflictos al momento del pull-request

--- src/data.js
+++ src/data.js
@@ -1,12 +1,5 @@
 export const showCharacterbyName = (data, cName) => {
   let characterSelected = data.characters.filter(element => element.name == cName);
+  console.log(characterSelected)
   return characterSelected;
-}
-
-export const showCharacterRealTime = (data) => {
-  let characterLower = data.characters.filter(element => element.name);
-  // let characterLower = characterSelected.toLowerCase();
-  console.log(characterLower);
-
-  return characterLower;
 }
