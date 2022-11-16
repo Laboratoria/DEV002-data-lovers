dccf7f73a0b5e6ec405c616016f40369299de051
vickymendoza <vickymendoza90@gmail.com>
2022-11-16 16:38:06 -0500

    Pintar los productores

--- src/data.js
+++ src/data.js
@@ -15,5 +15,21 @@ const operaciones = {
 
     return todosLosDirectores;
   },
+
+  //Esto es una función que recibe como argumento las películas
+  llamarProductores(peliculas) {
+    let pelicula;
+    let todosLosDirectores = [];
+    //En este ciclo for recorrimos el array de películas y luego con el if trajimos los directores pero no cada vez que estuvieran presentes sino que vinieran sólo una vez.
+    for (let i = 0; i < peliculas.length; i++) {
+      pelicula = peliculas[i];
+
+      if (!todosLosDirectores.includes(pelicula.director)) {
+        todosLosDirectores.push(pelicula.director);
+      }
+    }
+
+    return todosLosDirectores;
+  },
 };
 export default operaciones;
--- src/main.js
+++ src/main.js
@@ -1,20 +1,39 @@
 import operaciones from "./data.js";
 import data from "./data/ghibli/ghibli.js";
 
-//console.log(operaciones.llamarDirectores(data.films));
-
+//PINTA DIRECTORES
 function pintarDirectores() {
-  let directoresHtml = "<h2>DIRECTORES</h2>";
+  let directoresHtml = "<h2>Directores</h2>";
   directoresHtml += "<ul>";
 
   let arregloDirectores = operaciones.llamarDirectores(data.films);
 
   for (let i = 0; i < arregloDirectores.length; i++) {
-    console.log(arregloDirectores[i]);
+    directoresHtml += "<li>";
+    directoresHtml += arregloDirectores[i];
+    directoresHtml += "</li>";
   }
 
-  // console.log(arregloDirectores);
   directoresHtml += "</ul>";
-  directoresdiv.innerHTML += directoresHtml;
+  directoresproductoresdiv.innerHTML += directoresHtml;
 }
 console.log(pintarDirectores());
+
+//PINTA PRODUCTORES
+function pintarProductores() {
+  let directoresHtml = "<h2>Productores</h2>";
+  directoresHtml += "<ul>";
+
+  //CABIAS A llamarProductores
+  let arregloDirectores = operaciones.llamarDirectores(data.films);
+
+  for (let i = 0; i < arregloDirectores.length; i++) {
+    directoresHtml += "<li>";
+    directoresHtml += arregloDirectores[i];
+    directoresHtml += "</li>";
+  }
+
+  directoresHtml += "</ul>";
+  directoresproductoresdiv.innerHTML += directoresHtml;
+}
+console.log(pintarProductores());
