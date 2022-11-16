074cf6edb1b21c48e49c4c028ad23c4bca62b912
Domynike Galarce <dgalarcegonzalez@gmail.com>
2022-11-16 16:25:21 -0300

    borrar comentarios

--- src/data.js
+++ src/data.js
@@ -1,17 +1,12 @@
 export function getAllCharacters () {
 	return fetch("https://hp-api.herokuapp.com/api/characters")
 		.then((response) => {
-			// The API call was successful!
-			//console.log("success!", response);
 		return response.json();
 		})
 		.then((data) => {
-			// This is the JSON from our response
-			//console.log("yay", data[0]);
 		return data;
 		})
 		.catch((err) => {
-			// There was an error
 			console.warn("Something went wrong.", err);
 		})
 	}
--- src/main.js
+++ src/main.js
@@ -2,23 +2,15 @@ import {getAllCharacters} from "./data.js";
 
 
 
-// const data = dataTest();
 const inputs = document.querySelectorAll(".inpuuuut");
-//console.log("datos " + data);
-//console.log("input " + input);
 
 inputs.forEach(input =>{ 
 input.addEventListener("keydown", async (event) =>{  
   if (event.keyCode === 13){
-    //const inputDos = document.getElementsByClassName("inpuuuut");
-    //const keyInput = event.key;
-    //console.log("dos " +inputDos.value)
     const data = await getAllCharacters();
-    //console.log(data)
     const result = data.filter((characters) => {
         return characters.name.toLowerCase().includes(input.value)
     })
-    console.log("resultado " + input.value, result)
 
     let characterName = "";
     result.forEach(character => {
@@ -27,23 +19,7 @@ input.addEventListener("keydown", async (event) =>{
             <img class="character-image" src="${character.image || "assets/user.png"}"/>
             <h3 class="name">${character.name}</h3>
         </div>`
-        //() =>{
-            //if (character.image !== ""){
-              //  `<div class="character">
-                //    <img class="character-image" src="${character.image}"/>
-                  //  <h3>${character.name}</h3>
-                //</div>`;
-            //}
-            //else {
-              //  `<div class="character-no-image">
-                //    <img class="no-image-character" src="assets/user.png" alt="Image not found"/>
-                  //  <h3>${character.name}</h3>
-                //</div>`;
-            //}
-        //}
-        console.log("nombre " + characterName)
     })
-
 
     document.getElementById("results").innerHTML = characterName
   }
