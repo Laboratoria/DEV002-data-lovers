69dd764ab5f848c2f2fc9be803b40b2add53e867
Roxsy <roxsy.dj@gmail.com>
2022-11-16 18:21:15 -0500

    instalando dependencias npm

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/ghibli/ghibli.js';
+const API = './data/ghibli/ghibli.json';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+fetch(API)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
+
