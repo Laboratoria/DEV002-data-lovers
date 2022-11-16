700a3c0f5dbfd9a76630a9366886e81c8e9cc25d
Domynike Galarce <dgalarcegonzalez@gmail.com>
2022-11-16 16:09:23 -0300

    continuacion js + imagen personajes

--- src/main.js
+++ src/main.js
@@ -3,20 +3,51 @@ import {getAllCharacters} from "./data.js";
 
 
 // const data = dataTest();
-const input = document.getElementById("search");
-
+const inputs = document.querySelectorAll(".inpuuuut");
 //console.log("datos " + data);
 //console.log("input " + input);
 
+inputs.forEach(input =>{ 
 input.addEventListener("keydown", async (event) =>{  
-    const inputDos = document.getElementById("search");
+  if (event.keyCode === 13){
+    //const inputDos = document.getElementsByClassName("inpuuuut");
     //const keyInput = event.key;
     //console.log("dos " +inputDos.value)
     const data = await getAllCharacters();
     //console.log(data)
     const result = data.filter((characters) => {
-        return characters.name.toLowerCase().includes(inputDos.value)
+        return characters.name.toLowerCase().includes(input.value)
     })
-    console.log("resultado " + inputDos.value, result)
+    console.log("resultado " + input.value, result)
+
+    let characterName = "";
+    result.forEach(character => {
+        characterName += 
+        `<div class="character">
+            <img class="character-image" src="${character.image || "assets/user.png"}"/>
+            <h3 class="name">${character.name}</h3>
+        </div>`
+        //() =>{
+            //if (character.image !== ""){
+              //  `<div class="character">
+                //    <img class="character-image" src="${character.image}"/>
+                  //  <h3>${character.name}</h3>
+                //</div>`;
+            //}
+            //else {
+              //  `<div class="character-no-image">
+                //    <img class="no-image-character" src="assets/user.png" alt="Image not found"/>
+                  //  <h3>${character.name}</h3>
+                //</div>`;
+            //}
+        //}
+        console.log("nombre " + characterName)
+    })
+
+
+    document.getElementById("results").innerHTML = characterName
+  }
+})
 })
 
+//inactive se hace con diferentes inputs
