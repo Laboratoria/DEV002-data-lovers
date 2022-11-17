64bfa46ed475c53302a53963b1c2bdd1bc38404c
beresdev <beresdev@gmail.com>
2022-11-17 13:30:32 -0600

    Refactorización de código, buscador en tiempo real y ordenamiento ascendente y descendente

--- src/main.js
+++ src/main.js
@@ -1,124 +1,28 @@
-import {showCharacterbyName, showAutomatic } from './data.js';
+import {createArrayObjects, filterCompare, filterAscendent,filterDescendent} from './data.js';
 import data from './data/harrypotter/data.js';
 
-const searchI = document.querySelector('#search-character');
-const searchB = document.getElementById("search-button");
-let showCharacter;
-let colorHat;
+const searchInput = document.getElementById('search-character');
+const searchButton = document.getElementById("search-button");
+
 let list = document.getElementById("characteresList");
 let orderAscendent = document.getElementById("filterAscendent");
 let orderDescendent = document.getElementById("filterDescendent");
 
-searchB.addEventListener("click", searchButton);
-// searchI.addEventListener('keyup', searchButton);
+searchButton.addEventListener("click", searchButton);
+searchInput.addEventListener('keyup', searchRealTime);
 
-searchI.addEventListener('keyup', showRealTime);
-
-    function searchButton() {
-        let cName = searchI.value;
-        let characterFound = showCharacterbyName(data,cName);
-        list.innerHTML = '';
-
-        characterFound.forEach(element => {
-            if(element.house == "Gryffindor") {
-                    colorHat = "#740001";
-                } else if(element.house == "Hufflepuff") {
-                    colorHat = "#F0C75E";
-                } else if(element.house == "Slytherin" ) {
-                    colorHat = "#2A623D";
-                } else if(element.house == "Ravenclaw") {
-                    colorHat = "#728DDA";
-                } else {
-                    colorHat = "#000";
-                }
-                showCharacter = `
-                <div class="card">
-                    <span><i style="color: ${colorHat}"  class="fa-solid fa-hat-wizard"></i></span>
-                    <p>${element.name}</p>
-                </div>
-                `
-                list.innerHTML += showCharacter;
-        })
-    }
-
-// listElements(data.characters.map((character) => character.name));
+let charactersNameHouse = createArrayObjects(data);
 
 listElements(data.characters.filter(element => element.name));
 
-orderAscendent.addEventListener("click", () => {
-    list.innerHTML = '';
-    // let names = data.characters.filter((character) => character.name).sort();
-
-    // console.log(names.sort(function(a, b) {
-        //     return((a.name < b.name)? -1: ((a.name > b.name)? 1: 0));
-        // }));
-    let names = showAutomatic(data);
-    listElements(names.sort(function(a, b) {
-        return((a.name < b.name)? -1: ((a.name > b.name)? 1: 0));
-    }));
-});
-
-orderDescendent.addEventListener("click", () => {
-    list.innerHTML = '';
-    // let names = data.characters.filter((character) => character.name).sort().reverse();
-    // listElements(names);
-    let names = showAutomatic(data);
-    listElements(names.sort(function(a, b) {
-        return((a.name < b.name)? 1: ((a.name > b.name)? -1: 0));
-    }));
-
-});
-
 function listElements(elements) {
     elements.forEach((element) => {
-        // let li = document.createElement('li');
-        // let liHouses = document.createElement
-        // li.innerHTML = cardElement;
-        // li.className = 'card-element';
-        // list.appendChild(li);
-            let  div = document.createElement('div');
-            let span = document.createElement('span');
-            let i = document.createElement('i')
-            let p = document.createElement('p');
-    
-            div.className = 'card-element'
-            p.innerHTML = element.name;
-            p.className = 'card-p';
-            i.className = 'fa-solid fa-hat-wizard';
-            i.id = 'span-i';
-            span.className = 'card-span';
-    
-            if(element.house == "Gryffindor") {
-                span.style.color = "#740001";
-                } else if(element.house == "Hufflepuff") {
-                    span.style.color = "#F0C75E";
-                } else if(element.house == "Slytherin" ) {
-                    span.style.color = "#2A623D";
-                } else if(element.house == "Ravenclaw") {
-                    span.style.color = "#728DDA";
-                } else {
-                    span.style.color = "#000";
-                }
-    
-                list.appendChild(div)
-                div.appendChild(span);
-                span.appendChild(i)
-                div.appendChild(p);
-            })
-}
-
-function showRealTime() {
-    let cName = searchI.value;
-    let character = showAutomatic(data);
-    list.innerHTML = '';
-    character.filter(element => element.name.toLowerCase().includes(cName.toLowerCase().trim()))
-    .forEach(element => {
-        let  div = document.createElement('div');
+        let  li = document.createElement('li');
         let span = document.createElement('span');
         let i = document.createElement('i')
         let p = document.createElement('p');
-
-        div.className = 'card-element'
+    
+        li.className = 'card-element'
         p.innerHTML = element.name;
         p.className = 'card-p';
         i.className = 'fa-solid fa-hat-wizard';
@@ -127,22 +31,35 @@ function showRealTime() {
 
         if(element.house == "Gryffindor") {
             span.style.color = "#740001";
-            } else if(element.house == "Hufflepuff") {
-                span.style.color = "#F0C75E";
-            } else if(element.house == "Slytherin" ) {
-                span.style.color = "#2A623D";
-            } else if(element.house == "Ravenclaw") {
-                span.style.color = "#728DDA";
-            } else {
-                span.style.color = "#000";
-            }
+        } else if(element.house == "Hufflepuff") {
+            span.style.color = "#F0C75E";
+        } else if(element.house == "Slytherin" ) {
+            span.style.color = "#2A623D";
+        } else if(element.house == "Ravenclaw") {
+            span.style.color = "#728DDA";
+        } else {
+            span.style.color = "#000";
+        }
 
-
-            // i.style.color = colorHat;
-
-            list.appendChild(div)
-            div.appendChild(span);
-            span.appendChild(i)
-            div.appendChild(p);
-        })
+        list.appendChild(li);
+        li.appendChild(span);
+        span.appendChild(i);
+        li.appendChild(p);
+    })
 }
+
+function searchRealTime() {
+    let cName = searchInput.value;
+    list.innerHTML = '';
+    listElements(filterCompare(charactersNameHouse, cName));
+}
+
+orderAscendent.addEventListener("click", () => {
+    list.innerHTML = '';
+    listElements(filterAscendent(charactersNameHouse));
+});
+
+orderDescendent.addEventListener("click", () => {
+    list.innerHTML = '';
+    listElements(filterDescendent(charactersNameHouse));
+});
