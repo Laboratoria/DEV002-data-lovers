18ea2512ecc5ec693ac85b5023dbcdfe50a7d30a
beresdev <beresdev@gmail.com>
2022-11-17 23:26:51 -0600

    resolviendo conflictos #3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-import {showCharacterbyName, showCharacterRealTime} from './data.js';
-
+import {showCharacterbyName} from './data.js';
 import data from './data/harrypotter/data.js';
-
-console.log(data);
 
 const searchI = document.querySelector('#search-character');
 const searchB = document.getElementById("search-button");
@@ -10,15 +7,12 @@ const list = document.getElementById("characters-list-example");
 let showCharacter;
 let colorHat;
 
-// const colorHouses {
-//     Gryffindor: "#740001",
-//     Hufflepuff: "#F0C75E",
-//     Slytherin: "#2A623D",
-//     Ravenclaw: "#728DDA"
-// };
+
+
+
 
 searchB.addEventListener("click", searchButton);
-searchI.addEventListener('keyup', searchButton)
+searchI.addEventListener('keyup', searchButton);
 
     function searchButton() {
         let cName = searchI.value;
@@ -46,38 +40,3 @@ searchI.addEventListener('keyup', searchButton)
             list.innerHTML += showCharacter;
         })
     }
-
-    // function automaticSearch() {
-    //     //list.innerHTML = '';
-    //     let cName = searchI.value;
-    //     console.log(cName)
-    //     let charactersNames = showCharacterRealTime(data);
-    //     console.log(charactersNames)
-    //     // let res = charactersNames.name.includes(cName);
-    //     // console.log(res)
-
-    //     for(let character of charactersNames) {
-    //         console.log(character) 
-    //     }
-
-        // charactersNames.forEach(character => {
-        //     if(charactersNames.idexOf(cName) !== -1) {
-        //         list.innerHTML += `
-        //         <div class="card">
-        //             <span><i style="color: ${colorHat}"  class="fa-solid fa-hat-wizard"></i></span>
-        //             <p>${character.name}</p>
-        //         </div>
-        //         ` 
-        //     } else { 
-        //         list.innerHTML += `
-        //         <div class="card">
-        //             <p>Not found</p>
-        //         </div>
-        //         ` 
-        //     }
-        // })
-    // }
-
-    // searchI.addEventListener('keyup', automaticSearch)
-
-    // automaticSearch();
