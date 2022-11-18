e718ad7034afc704fef83abc4ffaeb4995dfa979
beresdev <beresdev@gmail.com>
2022-11-17 23:20:50 -0600

    Resolviendo conflictos #1

--- src/main.js
+++ src/main.js
@@ -1,64 +1,38 @@
-import { example, orderCharacters, printCharacters, printElements, showCharacterbyName } from './data.js';
+import {showCharacterbyName} from './data.js';
+
 import data from './data/harrypotter/data.js';
 
+console.log(data);
 
+const searchI = document.getElementById("search-character");
+const searchB = document.getElementById("search-button");
+const list = document.getElementById("characters-list-example");
+let showCharacter;
+let colorHat;
 
+searchB.addEventListener("click", function() {
+    let cName = searchI.value;
+    let characterFound = showCharacterbyName(data,cName);
+    console.log(characterFound)
 
-
-
-
-let list = document.getElementById("characteresList");
-let orderAscendent = document.getElementById("filterAscendent");
-let orderDescendent = document.getElementById("filterDescendent");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-listElements(data.characters.map((character) => character.name + character.house));
-orderAscendent.addEventListener("click", () => {
-    list.innerHTML = '';
-    let names = data.characters.map((character) => character.name).sort();
-    listElements(names);
-});
-orderDescendent.addEventListener("click", () => {
-    list.innerHTML = '';
-    let names = data.characters.map((character) => character.name).sort().reverse();
-    listElements(names);
-});
-
-function listElements(elements) {
-    elements.forEach((cardElement) => {
-        let li = document.createElement('li');
-        let liHouses = document.createElement
-        li.innerHTML = cardElement;
-        li.className = 'card-element';
-        list.appendChild(li);
+    characterFound.forEach(element => {
+        if(element.house == "Gryffindor") {
+            colorHat = "#740001";
+        } else if(element.house == "Hufflepuff") {
+            colorHat = "#F0C75E";
+        } else if(element.house == "Slytherin" ) {
+            colorHat = "#2A623D";
+        } else if(element.house == "Ravenclaw") {
+            colorHat = "#728DDA";
+        } else {
+            colorHat = "#000";
+        }
+        showCharacter = `
+        <div class="card">
+            <span><i style="color: ${colorHat}"  class="fa-solid fa-hat-wizard"></i></span>
+            <p>${element.name}</p>
+        </div>
+        `
+        list.innerHTML += showCharacter;
     })
-}
+    });
