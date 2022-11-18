d4f9a7f7d71838b6a87d8c39545e52c85237605a
Domynike Galarce <dgalarcegonzalez@gmail.com>
2022-11-18 17:18:55 -0300

    new character button and character result to the button

--- src/main.js
+++ src/main.js
@@ -5,25 +5,51 @@ import {getAllCharacters} from "./data.js";
 const inputs = document.querySelectorAll(".inpuuuut");
 
 inputs.forEach(input =>{ 
-input.addEventListener("keydown", async (event) =>{  
-  if (event.keyCode === 13){
-    const data = await getAllCharacters();
-    const result = data.filter((characters) => {
-        return characters.name.toLowerCase().includes(input.value)
-    })
+  input.addEventListener("keydown", async (event) =>{  
+    if (event.keyCode === 13){
+      let data = await getAllCharacters();
+      //data contiene el resultado de la promesa
+      //filter(función de arreglo) necesita una condición que se entrega con return. la condición entrega true o false
+      let result = data.filter((characters) => {
+          return characters.name.toLowerCase().includes(input.value)
+      })
+
+      let characterName = "";
+      result.forEach(character => {
+          characterName += 
+          `<div class="character">
+              <img class="character-image" src="${character.image || "assets/user.png"}"/>
+              <h3 class="name">${character.name}</h3>
+          </div>`
+      })
+
+      //obtener result y cambiarle el HTML
+      document.getElementById("results").innerHTML = characterName
+    }
+  })
+})
+
+const dotButton = document.getElementById("dotsButton");
+const characterButton = document.getElementById("charactersButton");
+
+dotButton.addEventListener("click", (e) => {
+  document.getElementById("dotsButton").hidden = true;
+  document.getElementById("charactersButton").classList.remove("inactive");
+})
+
+characterButton.addEventListener("click", async (show) => {
+  let data = await getAllCharacters();
+     //data contiene el resultado de la promesa
 
     let characterName = "";
-    result.forEach(character => {
-        characterName += 
-        `<div class="character">
+  data.forEach(character => {
+      characterName += 
+      `<div class="character">
             <img class="character-image" src="${character.image || "assets/user.png"}"/>
             <h3 class="name">${character.name}</h3>
         </div>`
-    })
+})
 
+//     //obtener result y cambiarle el HTML
     document.getElementById("results").innerHTML = characterName
-  }
 })
-})
-
-//inactive se hace con diferentes inputs
