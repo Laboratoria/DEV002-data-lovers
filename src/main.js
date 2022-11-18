da229988a306444ab65ec870a965ea4f380b6847
Roxsy <roxsy.dj@gmail.com>
2022-11-18 12:47:24 -0500

    consuminedo data main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
+
+
 import { example } from './data.js';
+import data from './data/ghibli/ghibli.js';
+
+
+
+for (const img in data) {
+  console.log(`${img}: ${data[img]}`);
+}
+console.log("esto es un ejemplo" , example, data, img);
+
+
 // import data from './data/lol/lol.js';
 //import data from './data/ghibli/ghibli.js';
-const API = './data/ghibli/ghibli.json';
-// import data from './data/rickandmorty/rickandmorty.js';
+
+/*const API = './data/ghibli/ghibli.json/films';
+import data from './data/rickandmorty/rickandmorty.js';
 
 
 fetch(API)
   .then(res => res.json())
   .then(data => {
     console.log(data);
-  });
+  }); */
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,40 @@
-import { example, anotherExample } from '../src/data.js';
+
+import { sumar } from '../src/data.js';
+
+describe('Pruebas para la funcion sumar', ()=> {
+  it('Deberia retornar 2 si sus argumentos son 1 y 1', ()=> {
+    const resultado = sumar(1, 1)
+
+    expect(resultado).toBe(2)
+  })
+
+  it('No deberia retornar 2 si sus argumentos son 1 y 4', ()=> {
+    const resultado = sumar(1, 4)
+
+    expect(resultado).not.toBe(2)
+  })
+
+  // toContain, toEqual, toMatch, not
+})
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
+//   it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
