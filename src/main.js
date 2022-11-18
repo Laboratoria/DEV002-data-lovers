87fd7dce51d45b23a9df5daec8b2997dc8507be7
Laura Buitrago <buitrago92943@gmail.com>
2022-11-17 21:02:57 -0500

    Actualizacion data

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/harry-data.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
-let containerSidenav = document.createElement("div");
-containerSidenav.setAttribute("class", "container-sidenav");
-
-console.log(example, data);
+console.log(data);
