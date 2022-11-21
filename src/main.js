d00aad9a8138243127fbc2dbc657ed3f668e4116
Adriana <adrianazambranoc7@gmail.com>
2022-11-21 12:18:19 -0500

    botones

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import { example } from './data.js';
-
-
 
 import data from './data/pokemon/pokemon.js';
+import { filtrar } from './data.js';
 
+
+
+
+
+  
+ document.getElementById("btnPokemones").addEventListener("click", () => {
+    document.querySelector('.paginaPrincipal').style.display= 'none';
+    document.querySelector('.paginaEstadistica').style.display= 'none';
+    document.querySelector('.paginaPokemones').style.display= 'block';
+
+});
+
+document.getElementById("btnEstadistica").addEventListener("click", () => {
+    document.querySelector('.paginaPrincipal').style.display= 'none';
+    document.querySelector('.paginaEstadistica').style.display= 'block';
+    document.querySelector('.paginaPokemones').style.display= 'none';
+
+});
 
 console.log(example, data);
+
