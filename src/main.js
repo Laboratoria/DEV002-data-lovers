712ac18af5aa7a3fc07d9255e8d0ac4c38449359
Roxsy <roxsy.dj@gmail.com>
2022-11-21 10:28:48 -0500

    mostrar nombre peliculas en DOM

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
 
 
-import { example } from './data.js';
+//import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
+let divt = document.getElementById("container");
+
+const filmst = data.films
+const app = document.getElementById('app')
+
+filmst.forEach(product => {
+  divt.innerHTML += `<li>${product.title} -  ${product.producer}</li>`
+})
 
 
 
-for (const img in data) {
-  console.log(`${img}: ${data[img]}`);
-}
-console.log("esto es un ejemplo" , example, data, img);
 
+
+
+
+const search = (query) => {
+  return filmst.filter(item => {
+    return item.title.includes(query) 
+  });
+};
+
+//console.log("cas", search("cast"))
+//app.innerHTML += `<li>${product.title} -$ ${product.price}</li>`
+//console.log("esto es un ejemplo" , example, data, img);
+//data.forEach(element => console.log("", element));
 
 // import data from './data/lol/lol.js';
 //import data from './data/ghibli/ghibli.js';
 
-/*const API = './data/ghibli/ghibli.json/films';
-import data from './data/rickandmorty/rickandmorty.js';
+//const API = './data/ghibli/ghibli.json/films';
 
 
-fetch(API)
+
+/*fetch(API)
   .then(res => res.json())
   .then(data => {
     console.log(data);
