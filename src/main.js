fc58abd4e626eed9fb6ad812593fe455d2921150
Ana Cecilia Toribio <anctoribio@gmail.com>
2022-11-21 20:25:39 -0500

    Añadi la función para mostrar datos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const container = document.querySelector("#containerPokemon");
+
+fetch("https://raw.githubusercontent.com/Laboratoria/DEV002-data-lovers/main/src/data/pokemon/pokemon.json")
+    .then(response => response.json())
+    .then(data => {
+        data.pokemon.map(pokemon =>
+            pokemon);
+
+    });
+
+
+
+
+// console.log(example, data);
