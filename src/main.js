4bf60f66e013bc064ba111887c0e91c52f8d49a5
Adriana <adrianazambranoc7@gmail.com>
2022-11-24 11:48:03 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -23,13 +23,18 @@ document.getElementById("btnEstadistica").addEventListener("click", () => {
 //console.log(example, data);
 
 //Tarjetas de pokemones
-
+const seccion = document.getElementById('seccionPokemones');
 const contenedor = document.getElementById('tarjetaPokemones');
 
 const mostrar = (pokemones)=>{
     pokemones.forEach(pokemon => {
-     
-        contenedor.innerHTML+= `<img src='${pokemon.img}' class='imgPokemon'></img>`
+
+        
+        seccion.appendChild(contenedor);
+        seccion.setAttribute('class', 'paginaPokemones');
+        contenedor.innerHTML+=  `<div>
+                                    <img src='${pokemon.img}' class='imgPokemon'>
+                                   </div>`
         document.getElementById('nombrePokemon').innerHTML+=`<h2 class='nombreP'> ${pokemon.name} </h2>`
 
     });
