c1f86cb465bca4e10766a70c2cae24fb9449114f
filletournesols <dgalarcegonzalez@gmail.com>
2022-11-25 20:31:10 -0300

    new main with sort

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import {getAllCharacters} from "./data.js";
 
-//código dentro de una función: TU le dices cuando se ejecuta, si está fuera, JS elige cuando se ejecuta
 // manejo inputs
 const inputs = document.querySelectorAll(".inputSearch");
-
-export const createCharactersHtml = (characters) =>{
+const createCharactersHtml = (characters) =>{
   let characterName = "";
   characters.forEach(character => {
     characterName +=
@@ -15,8 +13,7 @@ export const createCharactersHtml = (characters) =>{
   })
   return characterName
 }
-
-const characterNotFound = () => {
+const characterNotFound = (characters) => {
   let characterDataNotFound =
     `<div class="characterNotFound">
       <h3 class="notFound">No results for this search</h3>
@@ -25,57 +22,82 @@ const characterNotFound = () => {
 }
 
 inputs.forEach(input =>{
-  if (input) {
-    input.addEventListener("keydown", async (event) =>{
-      if (event.keyCode === 13){
-        document.getElementById("home").hidden = true;
-        document.getElementById("navSearcher").classList.remove("inactive");
-        //data contiene el resultado de la promesa
-        let data = await getAllCharacters();
-        //filter(función de arreglo) necesita una condición que se entrega con return. la condición entrega true o false
-        let result = data.filter((characters) => {
-            return characters.name.toLowerCase().includes(input.value.toLowerCase())
-        })
-        if (result == false){
-          const dataNotFound = characterNotFound(result);
-          document.getElementById("results").innerHTML = dataNotFound
-        } else {
-          const charactersHTML = createCharactersHtml(result);
-          document.getElementById("results").innerHTML = charactersHTML
-        }
+  input.addEventListener("keydown", async (event) =>{
+    if (event.keyCode === 13){
+
+      /* Reseting sort options */
+      const sortButtons = document.getElementsByName('sort-characters');
+      sortButtons.forEach(button => button.checked = false);
+
+      document.getElementById("home").hidden = true;
+      document.getElementById("navSearcher").classList.remove("inactive");
+      //data contiene el resultado de la promesa
+      let data = await getAllCharacters();
+      //filter(función de arreglo) necesita una condición que se entrega con return. la condición entrega true o false
+      let result = data.filter((characters) => {
+        return characters.name.toLowerCase().includes(input.value)
+      })
+      if (result == false){
+        const dataNotFound = characterNotFound(result);
+        document.getElementById("results").innerHTML = dataNotFound
+      } else {
+        window.localStorage.setItem('Character', JSON.stringify(result));
+        const charactersHTML = createCharactersHtml(result);
+        document.getElementById("results").innerHTML = charactersHTML
       }
-    })
-  }
+    }
+  })
 })
 
 // esconder los ... y mostrar characters
-export const dotButton = document.getElementById("dotsButton");
+const dotButton = document.getElementById("dotsButton");
 
-if (dotButton) {
-  dotButton.addEventListener("click", () => {
+dotButton.addEventListener("click", () => {
   document.getElementById("dotsButton").hidden = true;
   document.getElementById("charactersButton").classList.remove("inactive");
-  })
-}
+})
 
 // al hacer click en characters, se esconde home y se muestra el nav con los personajes (nombre e imagen)
 const characterButton = document.getElementById("charactersButton");
 
-if (characterButton) {
-  characterButton.addEventListener("click", async() => {
+characterButton.addEventListener("click", async() => {
   document.getElementById("home").hidden = true;
   document.getElementById("navSearcher").classList.remove("inactive");
   let data = await getAllCharacters();
+  window.localStorage.setItem('Character', JSON.stringify(data));
   const charactersHTML = createCharactersHtml(data);
   document.getElementById("results").innerHTML = charactersHTML
-  })
-}
+})
 
 // recargar página una vez que se presiona el logo y va a home
 const logoButtonNav = document.getElementById("logoButtonNav");
 
-if (logoButtonNav){
-  logoButtonNav.addEventListener("click", () => {
-    window.location.reload(logoButtonNav)
+logoButtonNav.addEventListener("click", () => {
+  window.location.reload(logoButtonNav)
+})
+
+/* Sort function */
+const sortABtn = document.querySelector('#a-z');
+const sortZBtn = document.querySelector('#z-a');
+
+export function sortingCharacters (charactersList){
+  const newCharactersSorted = charactersList.sort((a, b) => {
+    if(a.name < b.name) { return -1; }
+    if(a.name > b.name) { return 1; }
+    return 0
   })
+  return newCharactersSorted
+}
+
+sortABtn.onclick = () => {
+  const newCharacters = JSON.parse(window.localStorage.getItem('Character'));
+  const charactersHTML = createCharactersHtml(sortingCharacters(newCharacters));
+  document.getElementById("results").innerHTML = charactersHTML
+}
+
+sortZBtn.onclick = () => {
+  console.log('Ordename z - a')
+  const newCharacters = JSON.parse(window.localStorage.getItem('Character'));
+  const charactersHTML = createCharactersHtml(sortingCharacters(newCharacters).reverse());
+  document.getElementById("results").innerHTML = charactersHTML
 }
