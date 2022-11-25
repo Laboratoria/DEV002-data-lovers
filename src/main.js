42a5da8d9db9ff451a69cbda3ae4bcc5a5957bb2
filletournesols <dgalarcegonzalez@gmail.com>
2022-11-25 20:23:27 -0300

    nuevo test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import {getAllCharacters} from "./data.js";
 
+//código dentro de una función: TU le dices cuando se ejecuta, si está fuera, JS elige cuando se ejecuta
 // manejo inputs
 const inputs = document.querySelectorAll(".inputSearch");
-const createCharactersHtml = (characters) =>{
+
+export const createCharactersHtml = (characters) =>{
   let characterName = "";
   characters.forEach(character => {
     characterName +=
@@ -13,7 +15,8 @@ const createCharactersHtml = (characters) =>{
   })
   return characterName
 }
-export const characterNotFound = () => {
+
+const characterNotFound = () => {
   let characterDataNotFound =
     `<div class="characterNotFound">
       <h3 class="notFound">No results for this search</h3>
@@ -22,50 +25,57 @@ export const characterNotFound = () => {
 }
 
 inputs.forEach(input =>{
-  input.addEventListener("keydown", async (event) =>{
-    if (event.keyCode === 13){
-      document.getElementById("home").hidden = true;
-      document.getElementById("navSearcher").classList.remove("inactive");
-      //data contiene el resultado de la promesa
-      let data = await getAllCharacters();
-      //filter(función de arreglo) necesita una condición que se entrega con return. la condición entrega true o false
-      let result = data.filter((characters) => {
-          return characters.name.toLowerCase().includes(input.value)
-      })
-      if (result == false){
-        const dataNotFound = characterNotFound(result);
-        document.getElementById("results").innerHTML = dataNotFound
-      } else {
-        const charactersHTML = createCharactersHtml(result);
-        document.getElementById("results").innerHTML = charactersHTML
+  if (input) {
+    input.addEventListener("keydown", async (event) =>{
+      if (event.keyCode === 13){
+        document.getElementById("home").hidden = true;
+        document.getElementById("navSearcher").classList.remove("inactive");
+        //data contiene el resultado de la promesa
+        let data = await getAllCharacters();
+        //filter(función de arreglo) necesita una condición que se entrega con return. la condición entrega true o false
+        let result = data.filter((characters) => {
+            return characters.name.toLowerCase().includes(input.value.toLowerCase())
+        })
+        if (result == false){
+          const dataNotFound = characterNotFound(result);
+          document.getElementById("results").innerHTML = dataNotFound
+        } else {
+          const charactersHTML = createCharactersHtml(result);
+          document.getElementById("results").innerHTML = charactersHTML
+        }
       }
-    }
-  })
+    })
+  }
 })
 
 // esconder los ... y mostrar characters
-const dotButton = document.getElementById("dotsButton");
+export const dotButton = document.getElementById("dotsButton");
 
-dotButton.addEventListener("click", () => {
+if (dotButton) {
+  dotButton.addEventListener("click", () => {
   document.getElementById("dotsButton").hidden = true;
   document.getElementById("charactersButton").classList.remove("inactive");
-})
+  })
+}
 
 // al hacer click en characters, se esconde home y se muestra el nav con los personajes (nombre e imagen)
 const characterButton = document.getElementById("charactersButton");
 
-characterButton.addEventListener("click", async() => {
+if (characterButton) {
+  characterButton.addEventListener("click", async() => {
   document.getElementById("home").hidden = true;
   document.getElementById("navSearcher").classList.remove("inactive");
   let data = await getAllCharacters();
   const charactersHTML = createCharactersHtml(data);
   document.getElementById("results").innerHTML = charactersHTML
-})
+  })
+}
 
 // recargar página una vez que se presiona el logo y va a home
 const logoButtonNav = document.getElementById("logoButtonNav");
 
-logoButtonNav.addEventListener("click", () => {
-  window.location.reload(logoButtonNav)
-})
-
+if (logoButtonNav){
+  logoButtonNav.addEventListener("click", () => {
+    window.location.reload(logoButtonNav)
+  })
+}
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,15 +1,97 @@
-import {characterNotFound} from "../src/main";
+//import {dotButton} from "../src/main";
 
-test("Check create characters conection to html", () => {
+test("ocultar dotButton y mostrar characterButton", () => {
     document.body.innerHTML =
-    `<div class="characterNotFound">
-        <h3 class="notFound">No results for this search</h3>
-    </div>`;
-    require("./main.js");
+    `<button class="dotsButton" id="dotsButton" >...</button>;
+    <button class="charactersButton inactive" id="charactersButton">Characters</button>`;
+    require("../src/main");
 
-    const resultSection = document.getElementById("results");
+    const dotsButton = document.getElementById("dotsButton")
+    dotsButton.dispatchEvent(new Event("click"))
+    const characterButton = document.getElementById("charactersButton")
 
-    resultSection.value = characterNotFound;
-
-    expect(resultSection).toBe(characterNotFound)
+    expect(characterButton.classList.contains("inactive")).toBe(false);
 });
+
+// test("recarga página al presiona logo en nav", () => {
+//     document.body.innerHTML =
+//     `<section class="nav-searcher inactive" id="navSearcher"> <!--"inactive"-->
+//     <section class="nav-searcher-top">
+//         <div class= "searcher-top-logo">
+//           <button id="logoButtonNav">
+//             <img src="assets/logo-gold.png" alt="Harry's Cabinet">
+//           </button>
+//         </div>
+//         <div class="searcher-top-bar">
+//           <div class="searcher-top-bar-input">
+//             <input type="text" placeholder="Try 'Gryffindor'" id="searchTwo" class="inputSearch"/>
+//             <button>🔍</button>
+//         </div>
+//         </div>
+//     </section>
+//     <section class="nav-searcher-bottom">
+//         <div class="searcher-bottom-sort">
+//             <button>
+//               <img src="./assets/sort.svg" alt="sort button icon"/>
+//             </button>
+//         </div>
+//         <div class="searcher-bottom-buttons searcher-bottom-filter">
+//             <button>
+//               <img src="./assets/filter.svg" alt="filter button icon"/>
+//             </button>
+//         </div>
+//     </section>
+//     </section>`
+//     require("../src/main");
+
+//     const logoBtn = document.getElementById("logoButtonNav")
+//     logoBtn.dispatchEvent(new Event("click"))
+//     const navSection = document.getElementById("navSearcher")
+    
+//     expect(navSection.classList.contains("inactive")).toBe(true);
+// });
+
+describe('window.location', () => {
+    const { location } = window;
+
+    beforeAll(() => {
+        delete window.location;
+        window.location = { reload: jest.fn() };
+    });
+
+    afterAll(() => {
+        window.location = location;
+    });
+
+    it("mocks `reload`", () => {
+        expect(jest.isMockFunction(window.location.reload)).toBe(true);
+    });
+
+    it("redirigir a home", () => {
+        document.body.innerHTML =
+            `<button id="logoButtonNav">
+                <img src="assets/logo-gold.png" alt="Harry's Cabinet">
+            </button>`
+        require("../src/main");
+
+        const navBtn = document.getElementById("logoButtonNav")
+        navBtn.dispatchEvent(new Event("click"))
+        //window.location.reload();
+        expect(window.location.reload).toBe(true);
+    });
+});
+
+// test("cambiar html results por character", () => {
+//     const { createCharactersHtml } = require("../src/main");
+
+//     const inputs = document.querySelectorAll(".inputSearch")
+//     inputs.dispatchEvent(new Event("onclick", "keyCode === 13"))
+//     const resultSection = document.getElementById("results")
+
+//     expect(resultSection).toBe(createCharactersHtml);
+// });
+
+
+//describe, en lugar de test ayuda a agrupar distintos test relacionados
+//typeof no da tipo de elemento en html, si no que en JS
+//hay que decirle a jest que se corre web para cambiar de ambiente --env=jsdom
