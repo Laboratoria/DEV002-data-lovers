b8bec084cefd16b01953251ce21abdcdf8c9594d
Ariel-Navarro <arielknm@gmail.com>
2022-11-25 00:50:35 -0500

    primer filtrado de personajes

--- src/data.js
+++ src/data.js
@@ -1,8 +1,24 @@
+
 import data from './harrydata.js';
 
-export const sort = () => {
-  return 'element.name';
-};
+
+//  export const x= numeros.sort(function(a, b){return b - a})
+
+export const ave = (arr) => {
+  return arr.sort((nombre, otroNombre ) => {
+    const nom=nombre.name
+      const otro=otroNombre.name
+    if ( nom.toLowerCase() < otro.toLowerCase()){
+      return -1;
+    }
+    else {
+      return 0;
+    }
+})
+
+}
+
+
 
 export const anotherExample = () => {
   return 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,57 +1,347 @@
+import { ave } from './data.js';
 import data from './harrydata.js';
-console.log(data)
+
+
+
+
+const test1 = document.querySelector('.test');
+console.log(test1)
 
 const selectCharacters = document.querySelector('#chooseCharacters');
 console.log(selectCharacters)
 
-// const test1 = document.querySelector('.test');
-// console.log(test1)
+
+const selectSpells = document.querySelector('#segundoSelect');
+console.log(selectSpells)
+
+
+
+
+// Seleccionamos a los personajes de A-Z  --------------------------------
 
 selectCharacters.addEventListener('change', () => {
-  let valorOption = selectCharacters.value;
-  console.log(valorOption)
+let valorOption = selectCharacters.value;
+console.log(valorOption)
   if (valorOption == 'a-z') {
+    // const x= document.getElementById("test")
+    // x.innerHTML=""
     const accederAlDom = document.getElementById('home')
     accederAlDom.style.display = 'none';
     const cardConteiner = document.getElementById('cardConteiner');
     cardConteiner.style.display = 'block';
 
-      const spells=data.characters;
-     spells.forEach(element => {
+      // const personajes=data.characters;
+
+
+      const filtro= ave(data.characters)
+
+     filtro.forEach(element => {
       const createElement = document.createElement("div")
       createElement.setAttribute("class", 'contenedorCard');
       const templateTest = `
       <div id="template">
 
-        <img  class="imagen" src="${element.image}" >
+        ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
 
          <div class="caja">
-          <p class="data">Name: ${element.name}</p>
-          <p class="data">Species: ${element.species}</p>
-         <p class="data">Gender: ${element.gender}</p>
-          <p class="data">Ancestry: ${element.ancestry}</p>
-          <p class="data">Actor: ${element.actor}</p>
+            <p class="data">Name: ${element.name}</p>
+            <p class="data">Species: ${element.species}</p>
+            <p class="data">Gender: ${element.gender}</p>
+            <p class="data">Ancestry: ${element.ancestry}</p>
+            <p class="data">Actor: ${element.actor}</p>
+          </div>
+     </div>`;
 
-        </div>
-     </div>
-     
-      `;
       createElement.innerHTML = templateTest;
+
       test1.appendChild(createElement);
 
+      return
+    });
+
+  } else if (valorOption == 'z-a'){
+    const x= document.getElementById("test")
+    x.innerHTML=""
+    // const x=document.getElementById("cardConteiner")
+    // x.innerHTML=""
+    const accederAlDom = document.getElementById('home')
+    accederAlDom.style.display = 'none';
+    const cardConteiner = document.getElementById('cardConteiner');
+    cardConteiner.style.display = 'block';
+
+      const persoanjes=data.characters;
+     persoanjes.forEach(element => {
+      const createElement = document.createElement("div")
+      createElement.setAttribute("class", 'contenedorCard');
+      const templateTest = `
+      <div id="template">
+
+        ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
+
+         <div class="caja">
+            <p class="data">Name: ${element.name}</p>
+            <p class="data">Species: ${element.species}</p>
+            <p class="data">Gender: ${element.gender}</p>
+            <p class="data">Ancestry: ${element.ancestry}</p>
+            <p class="data">Actor: ${element.actor}</p>
+          </div>
+     </div>`;
+
+      createElement.innerHTML = templateTest;
+      test1.appendChild(createElement);
+      return
+    });
+  }else if (valorOption == 'alive'){
+   const x= document.getElementById("test")
+   x.innerHTML=""
+    const accederAlDom = document.getElementById('home')
+    accederAlDom.style.display = 'none';
+    const cardConteiner = document.getElementById('cardConteiner');
+    cardConteiner.style.display = 'block';
+
+      const persoanjes=data.characters;
+     persoanjes.forEach(element => {
+      const createElement = document.createElement("div")
+      createElement.setAttribute("class", 'contenedorCard');
+      const templateTest = `
+      <div id="template">
+
+        ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
+
+         <div class="caja">
+            <p class="data">Name: ${element.name}</p>
+            <p class="data">Species: ${element.species}</p>
+            <p class="data">Gender: ${element.gender}</p>
+            <p class="data">Ancestry: ${element.ancestry}</p>
+            <p class="data">Actor: ${element.actor}</p>
+          </div>
+     </div>`;
+
+      createElement.innerHTML = templateTest;
+      test1.appendChild(createElement);
+      return
+    });
+
+  } else if(valorOption == "dead"){
+   const x= document.getElementById("test")
+   x.innerHTML=""
+    const accederAlDom = document.getElementById('home')
+    accederAlDom.style.display = 'none';
+    const cardConteiner = document.getElementById('cardConteiner');
+    cardConteiner.style.display = 'block';
+
+      const persoanjes=data.characters;
+     persoanjes.forEach(element => {
+      const createElement = document.createElement("div")
+      createElement.setAttribute("class", 'contenedorCard');
+
+      const templateTest = `
+      <div id="template">
+
+        ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
+
+         <div class="caja">
+            <p class="data">Name: ${element.name}</p>
+            <p class="data">Species: ${element.species}</p>
+            <p class="data">Gender: ${element.gender}</p>
+            <p class="data">Ancestry: ${element.ancestry}</p>
+            <p class="data">Actor: ${element.actor}</p>
+          </div>
+     </div>`;
+
+      createElement.innerHTML = templateTest;
+      test1.appendChild(createElement);
+      return
+    });
+  }else if(valorOption == 'species'){
+    const x=document.getElementById("test")
+    x.innerHTML=""
+    const accederAlDom = document.getElementById('home')
+    accederAlDom.style.display = 'none';
+    const cardConteiner = document.getElementById('cardConteiner');
+    cardConteiner.style.display = 'block';
+
+      const persoanjes=data.characters;
+     persoanjes.forEach(element => {
+      const createElement = document.createElement("div")
+      createElement.setAttribute("class", 'contenedorCard');
+      const templateTest = `
+      <div id="template">
+
+        ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
+
+         <div class="caja">
+            <p class="data">Name: ${element.name}</p>
+            <p class="data">Species: ${element.species}</p>
+            <p class="data">Gender: ${element.gender}</p>
+            <p class="data">Ancestry: ${element.ancestry}</p>
+            <p class="data">Actor: ${element.actor}</p>
+          </div>
+     </div>`;
+
+      createElement.innerHTML = templateTest;
+      test1.appendChild(createElement);
+      return
     });
   }
 
+    // selectSpells.innerHTML = "";
 });
 
 
+// seleccionamos los hechizos de la Z-A  ---------------------------------
 
-// seleccionamos los hechizos de la A-Z
-const selectSpells = document.querySelector('#segundoSelect');
-console.log(selectSpells)
+// selectCharacters.addEventListener('change', () => {
+//   let valorOption = selectCharacters.value;
+//   console.log(valorOption)
+//   if (valorOption == 'z-a') {
+//     const accederAlDom = document.getElementById('home')
+//     accederAlDom.style.display = 'none';
+//     const cardConteiner = document.getElementById('cardConteiner');
+//     cardConteiner.style.display = 'block';
 
-const test1 = document.querySelector('.test');
-console.log(test1)
+//       const persoanjes=data.characters;
+//      persoanjes.forEach(element => {
+//       const createElement = document.createElement("div")
+//       createElement.setAttribute("class", 'contenedorCard');
+//       const templateTest = `
+//       <div id="template">
+
+//         ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
+
+//          <div class="caja">
+//             <p class="data">Name: ${element.name}</p>
+//             <p class="data">Species: ${element.species}</p>
+//             <p class="data">Gender: ${element.gender}</p>
+//             <p class="data">Ancestry: ${element.ancestry}</p>
+//             <p class="data">Actor: ${element.actor}</p>
+//           </div>
+//      </div>`;
+
+//       createElement.innerHTML = templateTest;
+//       test1.appendChild(createElement);
+
+//     });
+//   }
+//     // selectSpells.innerHTML = "";
+// });
+
+
+// seleccionamos a los personajes vivos  -----------------------------------
+
+// selectCharacters.addEventListener('change', () => {
+//   let valorOption = selectCharacters.value;
+//   console.log(valorOption)
+//   if (valorOption == 'alive') {
+//     const accederAlDom = document.getElementById('home')
+//     accederAlDom.style.display = 'none';
+//     const cardConteiner = document.getElementById('cardConteiner');
+//     cardConteiner.style.display = 'block';
+
+//       const persoanjes=data.characters;
+//      persoanjes.forEach(element => {
+//       const createElement = document.createElement("div")
+//       createElement.setAttribute("class", 'contenedorCard');
+//       const templateTest = `
+//       <div id="template">
+
+//         ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
+
+//          <div class="caja">
+//             <p class="data">Name: ${element.name}</p>
+//             <p class="data">Species: ${element.species}</p>
+//             <p class="data">Gender: ${element.gender}</p>
+//             <p class="data">Ancestry: ${element.ancestry}</p>
+//             <p class="data">Actor: ${element.actor}</p>
+//           </div>
+//      </div>`;
+
+//       createElement.innerHTML = templateTest;
+//       test1.appendChild(createElement);
+
+//     });
+//   }
+//     // selectSpells.innerHTML = "";
+// });
+
+
+// seleccionamos a los personajes muertos  -----------------------------------
+
+// selectCharacters.addEventListener('change', () => {
+//   let valorOption = selectCharacters.value;
+//   console.log(valorOption)
+//   if (valorOption == 'dead') {
+//     const accederAlDom = document.getElementById('home')
+//     accederAlDom.style.display = 'none';
+//     const cardConteiner = document.getElementById('cardConteiner');
+//     cardConteiner.style.display = 'block';
+
+//       const persoanjes=data.characters;
+//      persoanjes.forEach(element => {
+//       const createElement = document.createElement("div")
+//       createElement.setAttribute("class", 'contenedorCard');
+
+//       const templateTest = `
+//       <div id="template">
+
+//         ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
+
+//          <div class="caja">
+//             <p class="data">Name: ${element.name}</p>
+//             <p class="data">Species: ${element.species}</p>
+//             <p class="data">Gender: ${element.gender}</p>
+//             <p class="data">Ancestry: ${element.ancestry}</p>
+//             <p class="data">Actor: ${element.actor}</p>
+//           </div>
+//      </div>`;
+
+//       createElement.innerHTML = templateTest;
+//       test1.appendChild(createElement);
+
+//     });
+//   }
+//     selectSpells.innerHTML = "";
+// });
+
+// seleccionamos a los personajes por su especie--------------------
+
+// selectCharacters.addEventListener('change', () => {
+//   let valorOption = selectCharacters.value;
+//   console.log(valorOption)
+//   if (valorOption == 'species') {
+//     console.log('here')
+//     const accederAlDom = document.getElementById('home')
+//     accederAlDom.style.display = 'none';
+//     const cardConteiner = document.getElementById('cardConteiner');
+//     cardConteiner.style.display = 'block';
+
+//       const persoanjes=data.characters;
+//      persoanjes.forEach(element => {
+//       const createElement = document.createElement("div")
+//       createElement.setAttribute("class", 'contenedorCard');
+//       const templateTest = `
+//       <div id="template">
+
+//         ${element.image!=="" ? `<img class='imagen' src=${element.image}>` : `<img class='imagen' src='imag/incógnito.jpg'>`}
+
+//          <div class="caja">
+//             <p class="data">Name: ${element.name}</p>
+//             <p class="data">Species: ${element.species}</p>
+//             <p class="data">Gender: ${element.gender}</p>
+//             <p class="data">Ancestry: ${element.ancestry}</p>
+//             <p class="data">Actor: ${element.actor}</p>
+//           </div>
+//      </div>`;
+
+//       createElement.innerHTML = templateTest;
+//       test1.appendChild(createElement);
+
+//     });
+//   }
+//     selectSpells.innerHTML = "";
+// });
+
+// seleccionamos los HECHIZOS de la A-Z  -----------------------------------
 
 selectSpells.addEventListener('change', () => {
   let valorOption = selectSpells.value;
@@ -85,4 +375,3 @@ selectSpells.addEventListener('change', () => {
   }
 
 });
-
