b3dce102e7b35391e69fdc620c58e6f88230db4f
vickymendoza <vickymendoza90@gmail.com>
2022-11-29 15:27:42 -0500

    organizando el desorden. creación del botón, creación de la función para ordenar la data y la implementación de la misma

--- src/data.js
+++ src/data.js
@@ -1,35 +1,6 @@
 //esto es un objeto que adentro tiene constantes y funciones
 const operaciones = {
-  //Esto es una función que recibe como argumento las películas
-  llamarDirectores(peliculas) {
-    let pelicula;
-    let todosLosDirectores = [];
-    //En este ciclo for recorrimos el array de películas y luego con el if trajimos los directores pero no cada vez que estuvieran presentes sino que vinieran sólo una vez.
-    for (let i = 0; i < peliculas.length; i++) {
-      pelicula = peliculas[i];
-
-      if (!todosLosDirectores.includes(pelicula.director)) {
-        todosLosDirectores.push(pelicula.director);
-      }
-    }
-
-    return todosLosDirectores;
-  },
-
-  //Esto es una función que recibe como argumento las películas
-  llamarProductores(peliculas) {
-    let pelicula;
-    let todosLosProductores = [];
-    //En este ciclo for recorrimos el array de películas y luego con el if trajimos los directores pero no cada vez que estuvieran presentes sino que vinieran sólo una vez.
-    for (let i = 0; i < peliculas.length; i++) {
-      pelicula = peliculas[i];
-      if (!todosLosProductores.includes(pelicula.producer)) {
-        todosLosProductores.push(pelicula.producer);
-      }
-    }
-
-    return todosLosProductores;
-  },
+  
 
   //ordenar por directores
   peliculasOrdenadasPorDirector(peliculas) {
