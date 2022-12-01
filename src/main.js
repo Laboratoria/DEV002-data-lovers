b745f8f58da013ee94a64703b60df4dafaa49104
Alexandra <antayalexandra@gmail.com>
2022-12-01 13:18:03 -0500

    probando data

--- src/main.js
+++ src/main.js
@@ -7,8 +7,17 @@ import data from './data/ghibli/ghibli.js';
 //console.log (data.films[0].poster)
 
 const prueba = document.getElementById("prueba")
-prueba.innerHTML = ` 
-<p class= "prueba"> ${data.films[0].director} ${data.films[0].title}</p>
-<img src="${data.films[0].poster}" />`
+//prueba.innerHTML = ` 
+//<div class= "prueba"> ${data.films[0].director} ${data.films[0].title}</div>
+//<img src="${data.films[0].poster}" />`
 
-console.log (data.films.forEach(element => console.log(element)))
+data.films.forEach(element=> {
+    prueba.innerHTML += ` 
+    <div class= "prueba"> Director: ${element.director} Título: ${element.title} ${element.description} ${element.release_date} ${element.rt_score}</div>
+    <img src="${element.poster}"/>`
+
+
+     
+})
+
+//console.log (data.films.forEach(element => console.log(element)))
