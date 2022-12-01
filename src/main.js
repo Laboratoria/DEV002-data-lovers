25c85d34282c2e22a4f9eadf5469c846ec5c0a90
Paola Quiroga <paolaquirogap10@gmail.com>
2022-11-30 22:56:10 -0500

    funciones separadas y un test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import data from './data/pokemon/pokemon.js';
-export { listarPokemon, pokemon, filtrados }
+import { listarPokemon, ordenarPor, porTipo, debilidades, resistencia } from './data.js'
 
 const headerViewOne = document.getElementsByClassName("divViewHeaderOne")[0];
 const headerViewTwo = document.getElementsByClassName("divViewHeaderTwo")[0];
@@ -11,38 +10,39 @@ const botonTipo = document.getElementById('botonTipo');
 const botonDebilidades = document.getElementById('botonDebilidades');
 const botonResistencia = document.getElementById('botonResistencia');
 
-bKanto.addEventListener('click', function () { listarPokemon('kanto') });
-bJohto.addEventListener('click', function () { listarPokemon('johto') });
+bKanto.addEventListener('click', () => {
+  headerViewOne.style.display = 'none';
+  headerViewTwo.style.display = 'block';
+  filtrados = listarPokemon('kanto');
+  pintarLista();
+});
+bJohto.addEventListener('click', () => {
+  headerViewOne.style.display = 'none';
+  headerViewTwo.style.display = 'block';
+  filtrados = listarPokemon('johto');
+  pintarLista();
+});
 botonOrdenarPor.addEventListener('change', () => {
-  ordenarPor(botonOrdenarPor.value);
+  filtrados = ordenarPor(botonOrdenarPor.value, filtrados);
+  pintarLista();
 });
 botonTipo.addEventListener('change', () => {
-  porTipo(botonTipo.value);
+  filtrados = porTipo(botonTipo.value, filtrados);
+  pintarLista();
 });
 botonDebilidades.addEventListener('change', () => {
-  debilidades(botonDebilidades.value);
+  filtrados = debilidades(botonDebilidades.value, filtrados);
+  pintarLista();
 });
 botonResistencia.addEventListener('change', () => {
-  resistencia(botonResistencia.value);
+  filtrados = resistencia(botonResistencia.value, filtrados);
+  pintarLista();
 });
 
 
 headerViewTwo.style.display = 'none';
 let filtrados;
 
-function listarPokemon(region) {
-  headerViewOne.style.display = 'none';
-  headerViewTwo.style.display = 'block';
-
-  divLista.innerHTML = "";
-  // Trae todos los pokemon de la region que seleccione
-  filtrados = data.pokemon.filter(p => p.generation.name === region);
-  for (let index = 0; index < filtrados.length; index++) {
-    //Trae cada pokemon de uno en uno = element
-    const element = filtrados[index];
-    divLista.innerHTML += pokemon(element.name, element.num, element.type, element.img)
-  }
-}
 // Por cada pokemon añade el return 
 let pokemon = (nombre, numero, tipo, imagen) => {
   return `<div class="contenedor">
@@ -57,20 +57,8 @@ let pokemon = (nombre, numero, tipo, imagen) => {
   </div>`
 }
 
-function ordenarPor(seleccion) {
-  if ("aZOrder" === seleccion) {
-    filtrados.sort((a, b) => a.name.localeCompare(b.name));
-  }
-  else if ("zAOrder" === seleccion) {
-    filtrados.sort((a, b) => b.name.localeCompare(a.name));
-  }
-  else if ("numAsc" === seleccion) {
-    filtrados.sort((a, b) => a.num - b.num);
-  }
-  else if ("numDes" === seleccion) {
-    filtrados.sort((a, b) => b.num - a.num);
-  }
-  // Limpia la lista de pokemom
+let pintarLista = () => {
+  // Limpia la lista de pokemoms
   divLista.innerHTML = "";
   // Agrega nuevamente los pokemon a la lista 
   for (let index = 0; index < filtrados.length; index++) {
@@ -79,40 +67,3 @@ function ordenarPor(seleccion) {
     divLista.innerHTML += pokemon(element.name, element.num, element.type, element.img)
   }
 }
-
-function porTipo(seleccion) {
-  filtrados = filtrados.filter(p => p.type.includes(seleccion));
-  divLista.innerHTML = "";
-  // Agrega nuevamente los pokemon a la lista 
-  for (let index = 0; index < filtrados.length; index++) {
-    //Trae cada pokemon de uno en uno = element
-    const element = filtrados[index];
-    divLista.innerHTML += pokemon(element.name, element.num, element.type, element.img)
-  }
-}
-
-function debilidades(seleccion) {
-  filtrados = filtrados.filter(p => p.weaknesses.includes(seleccion));
-  divLista.innerHTML = "";
-  // Agrega nuevamente los pokemon a la lista 
-  for (let index = 0; index < filtrados.length; index++) {
-    //Trae cada pokemon de uno en uno = element
-    const element = filtrados[index];
-    divLista.innerHTML += pokemon(element.name, element.num, element.type, element.img)
-  }
-}
-
-function resistencia(seleccion) {
-  filtrados = filtrados.filter(p => p.resistant.includes(seleccion));
-  divLista.innerHTML = "";
-  // Agrega nuevamente los pokemon a la lista 
-  for (let index = 0; index < filtrados.length; index++) {
-    //Trae cada pokemon de uno en uno = element
-    const element = filtrados[index];
-    divLista.innerHTML += pokemon(element.name, element.num, element.type, element.img)
-  }
-}
-
-
-
-console.log(data.pokemon.filter(p => p.generation.name === "johto"));
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,24 +1,20 @@
-import { listarPokemon, pokemon } from '../src/main';
+import { listarPokemon } from '../src/data';
 
 
-describe('Filtro por región', () => {
-  it('listarPokemon tipo funcion', () => {
+describe('Pruebas para la funcion Filtro por región', () => {
+  it('Validar que listarPokemon sea tipo funcion', () => {
     expect(typeof listarPokemon).toEqual('function');
   });
 
-  it('returns example pokemon', () => {
-    expect(pokemon('paola', '1', 'hada', 'img')).toEqual(`<div class="contenedor">
-    <div class="pokemon">
-      <img src="img" class="imagen" />
-      <div>
-        <p>paola</p>
-        <p>#1</p>
-        <p>hada</p>
-      </div>
-    </div>
-  </div>`)
-  });
+  it('validar que listarPokemon traiga la region kanto', () => {
+    expect(listarPokemon('kanto').length).toEqual(151);
+  }
+  
+  )
+
 });
+
+
 
 
 /*describe('anotherExample', () => {
