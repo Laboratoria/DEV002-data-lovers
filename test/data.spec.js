d0f3dd2eeb1ad8d51b27e466ee593c926fac8478
vickymendoza <vickymendoza90@gmail.com>
2022-12-01 11:49:00 -0500

    Test  función peliculas por año

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,10 +3,19 @@ import data from "../src/data/ghibli/ghibli.js";
 
 describe('test by operaciones', () => {
   it('should by a function', () => {
-    expect(typeof operaciones.peliculasOrdenadasPorDirector,operaciones.peliculasOrdenadasPorAno).toBe('function');
+    expect(typeof operaciones.peliculasOrdenadasPorDirector).toBe('function');
   });
 
   it('should return array', () => {
-    expect(Array.isArray(operaciones.peliculasOrdenadasPorDirector,operaciones.peliculasOrdenadasPorAno(data.films))).toBe(Boolean)
+    expect(Array.isArray(operaciones.peliculasOrdenadasPorDirector(data.films))).toBe(true)
+  });
+  describe('test by operaciones', () => {
+    it('should by a function', () => {
+      expect(typeof operaciones.peliculasOrdenadasPorAno).toBe('function');
+    });
+  
+    it('should return array', () => {
+      expect(Array.isArray(operaciones.peliculasOrdenadasPorAno(data.films))).toBe(false)
+    });
   });
 });
