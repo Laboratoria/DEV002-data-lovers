237788697e94c268851ca30ad81810dc18cab7e8
Alexandra <antayalexandra@gmail.com>
2022-12-01 17:24:16 -0500

    pantalla inicio

--- src/main.js
+++ src/main.js
@@ -11,13 +11,22 @@ const prueba = document.getElementById("prueba")
 //<div class= "prueba"> ${data.films[0].director} ${data.films[0].title}</div>
 //<img src="${data.films[0].poster}" />`
 
-data.films.forEach(element=> {
+data.films.forEach(element => {
     prueba.innerHTML += ` 
     <div class= "prueba"> Director: ${element.director} Título: ${element.title} ${element.description} ${element.release_date} ${element.rt_score}</div>
     <img src="${element.poster}"/>`
 
 
-     
+
 })
+
+let inicio = document.getElementById("peliculas");
+inicio.addEventListener("click", Mostrarpantalla2);
+function Mostrarpantalla2() {
+    let pantalla1 = document.getElementById("pantalla1")
+    pantalla1.style.display = "none";
+    let pantalla2 = document.getElementById("pantalla2");
+    pantalla2.style.display = "inline-flex";
+}
 
 //console.log (data.films.forEach(element => console.log(element)))
