9fc66910a3528a57816f76218202ef7edd3202ce
Erika Suesca <erikasuesca1234@gmail.com>
2022-12-03 17:16:03 -0500

    Cambios en main y data

--- src/main.js
+++ src/main.js
@@ -1,37 +1,42 @@
-
-import data from './data/pokemon/pokemon.js';
-import {  } from './data.js';
 import pokemon from './data/pokemon/pokemon.js';
-import { filtrarTipo } from './data.js';
-
-
+import { filtrarTipo,  ordenarData, buquedaPorNombre} from './data.js';
 
 // Funciones para visualizar las secciones del html: 
  document.getElementById("btnPokemones").addEventListener("click", () => {
     document.querySelector('.paginaPrincipal').style.display= 'none';
     document.querySelector('.paginaEstadistica').style.display= 'none';
     document.querySelector('.paginaPokemones').style.display= 'block';
-    document.querySelector('.filtrosBusqueda').style.display= 'block';
-
+    document.querySelector('.tiposInput').style.display= 'block';
+    document.querySelector('.filtrosBusqueda').style.display= 'block'; 
 });
 
 document.getElementById("btnEstadistica").addEventListener("click", () => {
     document.querySelector('.paginaPrincipal').style.display= 'none';
     document.querySelector('.paginaEstadistica').style.display= 'block';
     document.querySelector('.paginaPokemones').style.display= 'none';
-    document.querySelector('.filtrosBusqueda').style.display= 'none'
+    document.querySelector('.tiposInput').style.display= 'none';
+    document.querySelector('.filtrosBusqueda').style.display= 'none';
 });
 
+const todoslosPokemones = pokemon.pokemon;
+const inputBuscarNombre = document.getElementById('inputBuscarNombre');
+const botonFiltrar = document.getElementById('btn-filtrar');
+const ordenPokemonAz = document.getElementById('buttonAZ');
+const ordenPokemonZa = document.getElementById('buttonZA');
 
-  const seccion = document.getElementById('seccionPokemones');
-  seccion.setAttribute('class', 'paginaPokemones');
- const contenedorPadre= document.createElement('div');
- contenedorPadre.setAttribute('class','contenedorP');
- const contenedor = document.createElement('div');
-contenedor.setAttribute('class','contenedorTarjetas'); 
+const seccion = document.getElementById('seccionPokemones');
+seccion.setAttribute('class', 'paginaPokemones');
+
+const contenedorPadre = document.createElement('div');
+contenedorPadre.setAttribute('class', 'contenedorP');
+
+const contenedor = document.createElement('div');
+contenedor.setAttribute('class', 'contenedorTarjetas');
+
 const filtros = document.createElement('div');
-filtros.setAttribute('class','contenedorFiltros'); 
-contenedorPadre.appendChild(contenedor);     
+filtros.setAttribute('class', 'contenedorFiltros');
+
+contenedorPadre.appendChild(contenedor);
 contenedorPadre.appendChild(filtros);
 seccion.appendChild(contenedorPadre);
 
@@ -43,91 +48,58 @@ seccion.appendChild(contenedorPadre);
 
 // };
 
+const mostrar = (pokemones) => {
 
-const mostrar = (pokemones)=>{
-    pokemones.forEach(pokemon => {
-         
-            
-       contenedor.innerHTML+=  
-       
-       ` <div class="tarjetaPokemones" id="tarjetaPokemones" >
-                                <h2 id="nombrePokemon" class="nombreP">${pokemon.name} </h2>
-                                <h3 id="numeroPokemon" class="textoh3">${pokemon.num} </h3>
-                                 <div class="encabezadoTarjeta">                     
-                                  <img src="${pokemon.img}" alt="" id="imagenPokemon" class="imgPokemon">
-                                  <div class = 'contenedorh3'> <h3 id="tipoPokemon" class="textoh3">Type: ${pokemon.type} </h3>
-                                 <h3 id="generacionPokemon" class="textoh3"> Generation:${pokemon.generation.name} </h3>
-                                 </div>      
-                            </div>
-                            
-                                 <div class="descripcionTarjeta">
-                                   <p id="heightPokemon" class="sizeP"> height: ${pokemon.size.height}</p>
-                                   <p id="weightPokemon" class="sizeP"> weight: ${pokemon.size.weight}</p>
-                                 
-                                 </div>
-                                 
-                                 <div class="botonesTarjeta">
-                                   <button id="btnStats" class="bntS"> Stats </button>
-                                    
-                                   <button id="btnResistencia" class="btnR">Characteristics
-                                   </button>
-                                 </div>
-                                                      
-                                 </div>  `                        
-     
-    
+  pokemones.forEach((pokemon) => {
+    contenedor.innerHTML +=  
+        ` <div class="tarjetaPokemones" id="tarjetaPokemones" >
+             <h2 id="nombrePokemon" class="nombreP">${pokemon.name} </h2>
+             <h3 id="numeroPokemon" class="textoh3">${pokemon.num} </h3>
+              <div class="encabezadoTarjeta">                     
+                <img src="${pokemon.img}" alt="" id="imagenPokemon" class="imgPokemon">
+                <div class = 'contenedorh3'> <h3 id="tipoPokemon" class="textoh3">Type: ${pokemon.type} </h3>
+              <h3 id="generacionPokemon" class="textoh3"> Generation:${pokemon.generation.name} </h3>
+            </div>      
+            </div>
+            <div class="descripcionTarjeta">
+              <p id="heightPokemon" class="sizeP"> height: ${pokemon.size.height}</p>
+              <p id="weightPokemon" class="sizeP"> weight: ${pokemon.size.weight}</p>
+            </div>
+            <div class="botonesTarjeta">
+              <button id="btnStats" class="bntS"> Stats </button>
+              <button id="btnResistencia" class="btnR">Characteristics</button>
+            </div>
+          </div>  `
     });
 } 
-mostrar(pokemon.pokemon);
- 
-// Mostrar pokemones Filtrados:
-const todoslosPokemones= pokemon.pokemon;
 
-const botonFiltrar = document.getElementById('btn-filtrar');
 
-botonFiltrar.addEventListener('click',()=>{
+mostrar(todoslosPokemones);
 
+botonFiltrar.addEventListener('click', () => {
   document.getElementById("tipoPokemones").addEventListener("change", () => {
-    let seleccionTipoPokemon =document.getElementById('tipoPokemones').value;
+    let seleccionTipoPokemon = document.getElementById('tipoPokemones').value;
     if (seleccionTipoPokemon == "") {
       mostrar(pokemon.pokemon);
-    } 
-    else {
-      contenedor.innerHTML=''; 
-      const dataTipoPokemon = filtrarTipo(todoslosPokemones, seleccionTipoPokemon);     
-      mostrar(dataTipoPokemon);
-     
-      
     }
-  
+    else {
+      contenedor.innerHTML = '';
+      const dataTipoPokemon = filtrarTipo(todoslosPokemones, seleccionTipoPokemon);
+      mostrar(dataTipoPokemon);
+    }
   });
-  
-
 });
 
+ordenPokemonAz.addEventListener("click", () => {
+ contenedor.innerHTML = '';
+ mostrar(ordenarData(todoslosPokemones, 'name', 'asc'));
+});
 
- 
+ordenPokemonZa.addEventListener("click", () => {
+  contenedor.innerHTML = '';
+  mostrar(ordenarData(todoslosPokemones, 'name', 'desc'));
+});
 
-
-
-
-
-
-
-//  let tipoPokemon= getSelection('')
-// document.getElementById("tipoPokemon").value=
-// const filtrarTipo = (pokemonestipo) => {
-//   Object.entries(pokemon.pokemon).forEach( valor=> valor.forEach(dentro =>{
-//     let datosPokemones= dentro;
-//     const filtroTipos= datosPokemones.filter= (pokemones=> type['']=water)
-    
-//     console.log(filtroTipos)
-//   }
-//   ));
- 
-//   const tipoPokemones= datosPokemones.filter(tipo=>{
-//   return tipo.type === type['grass'];
-  
-// });
-// console.log(tipoPokemones)
-
+inputBuscarNombre.addEventListener("input",() => {
+  buquedaPorNombre (todoslosPokemones,inputBuscarNombre.value );
+});
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,57 @@
-import { example, anotherExample } from '../src/data.js';
+import {  filtrarTipo,  ordenarData} from '../src/data.js';
 
+const informacionPokemon= [
+  {
+  "num": "001",
+  "name": "bulbasaur",
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+  "type": [
+    "grass",
+    "poison"
+  ]
+},
+{
+  "num": "006",
+  "name": "charizard",
+    "type": [
+    "fire",
+    "flying"
+  ],
+},
+{
+  "num": "007",
+    "name": "squirtle",
+    
+      "type": [
+     "water"
+    ]
+}    
+  ]
+
+describe ('La funcion filtrarTipo retorna por tipo de pokemon', ()=>{
+  it('deberia ser una funcion', () => {
+    expect(typeof filtrarTipo).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('Debería retorna el Pokemon cuando es tipo grass', () => {
+    expect(filtrarTipo(informacionPokemon, "grass")).toHaveLength(1);
+  });
+  it('Debería retorna el Pokemon cuando es tipo water', () => {
+    expect(filtrarTipo(informacionPokemon, "water")).toHaveLength(1);
+  });
+});
+
+describe ('La funcion ordenarData me devuelve los pokemones de AZ Y ZA', ()=>{
+  it('deberia ser una funcion', () => {
+    expect(typeof ordenarData).toBe('function');
+  });
+
+  it('debería estar en la posición 1', () =>{
+    const resultado = ordenarData(informacionPokemon, "name");
+    expect(resultado[1].name).toEqual("charizard");
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+
