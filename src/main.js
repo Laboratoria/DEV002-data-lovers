f9043cc8800f67987b3464ae59c822593f6e08b5
Ginessa Fielstedt <gfielstedt@gmail.com>
2022-12-06 07:03:20 -0300

    impresión de data en pantalla

--- src/main.js
+++ src/main.js
@@ -3,4 +3,27 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log("example esto es la data", data);
+
+const prueba = document.getElementById("prueba")
+const dataPokemon = data.pokemon
+
+const displeyCard = (d) => {
+    d.forEach(element=>{
+        const cardPokemon = document.createElement("div")
+        cardPokemon.innerHTML +=`<section> 
+        <img src ="${element.img}">
+        <p>${element.num}</p>
+        </section>
+        <h2>${element.name}</h2>
+        
+        `
+        prueba.appendChild(cardPokemon);
+    })
+}
+
+displeyCard(dataPokemon)
+
+
+
+
