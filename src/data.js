86b084a4704466fc587addbed9e1caeffa5f5573
Daniela Castro <dgonzalezcs@outlook.com>
2022-12-06 10:35:50 -0600

    commit para merge #2. agregue favicon y main/data vacios para evitar conflictos al hacer merge con el codigo de mi dupla

--- src/data.js
+++
@@ -1,9 +0,0 @@
-// estas funciones son de ejemplo
-
-export const filtro = (arr = []) => {
-  return arr.filter((element) => element.house === 'Gryffindor');
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
