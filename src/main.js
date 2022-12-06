eb8d19494617a682091e535dd572bdaf3932eb4e
Ginessa Fielstedt <gfielstedt@gmail.com>
2022-12-06 20:56:49 -0300

    imprime los primeros 151

--- src/main.js
+++ src/main.js
@@ -1,29 +1,47 @@
-import { example } from './data.js';
+import {
+  example
+} from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log("example esto es la data", data);
 
+//mostrar
+
 const prueba = document.getElementById("prueba")
 const dataPokemon = data.pokemon
 
 const displeyCard = (d) => {
-    d.forEach(element=>{
-        const cardPokemon = document.createElement("div")
-        cardPokemon.innerHTML +=`<section> 
+  d.forEach(element => {
+
+    const cardPokemon = document.createElement("div")
+
+    cardPokemon.innerHTML += ` <section class= "flex-cont"> <section class= "flex-box">
         <img src ="${element.img}">
         <p>${element.num}</p>
+        <section> <h2>${element.name}</h2></div></section>
         </section>
-        <h2>${element.name}</h2>
-        
+        </section>
+
         `
-        prueba.appendChild(cardPokemon);
-    })
+    prueba.appendChild(cardPokemon);
+
+  })
 }
 
-displeyCard(dataPokemon)
+//displeyCard(dataPokemon)
 
+//filter
 
+let generationI = dataPokemon.filter ((dataPokemon) => {
+    return dataPokemon.num <= 151;
+});
+//console.log(generationI)
 
+let generationII = dataPokemon.filter ((dataPokemon) => {
+      return dataPokemon.num > 151;
+});
+//console.log(generationII)
+displeyCard(generationI)
 
