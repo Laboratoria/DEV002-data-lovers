d85a392c93727f43f6433e2c126202927ca1b835
saamcruzzr <contacto.samcruz@gmail.com>
2022-12-08 10:34:37 -0600

    mostrar data. fotos

--- src/main.js
+++ src/main.js
@@ -4,14 +4,26 @@ import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+const tarjetas = document.getElementById("tarjetas");
+data.films.forEach(element => {
+    const tarjeta = `
+    <div class="contenedor"> 
+          <section class="imagen"><figure><img src="${element.poster}"</figure></section>
+          <section class="texto">
+            <p id="titulo"></p>
+            <p id="datos"></p>
+          </section>
+    </div>
+    `
+    tarjetas.innerHTML += tarjeta
+});
 
+// const poster = document.getElementById("poster")
+// poster.innerHTML = ` <figure><img src="${data.films[0].poster}"</figure> `
 
-const poster = document.getElementById("poster")
-poster.innerHTML = ` <figure><img src="${data.films[0].poster}"</figure> `
+// const titulo = document.getElementById("titulo")
+// titulo.innerHTML = ` <p id= "titulo"> ${data.films[0].title} </p> `
 
-const titulo = document.getElementById("titulo")
-titulo.innerHTML = ` <p id= "titulo"> ${data.films[0].title} </p> `
-
-const datos = document.getElementById("datos")
-datos.innerHTML = ` <p id= "datos"> Director: ${data.films[0].director} <br> Productor: ${data.films[0].producer} <br> Lanzamiento: ${data.films[0].release_date} <br> Score: ${data.films[0].rt_score}</p>`
+// const datos = document.getElementById("datos")
+// datos.innerHTML = ` <p id= "datos"> Director: ${data.films[0].director} <br> Productor: ${data.films[0].producer} <br> Lanzamiento: ${data.films[0].release_date} <br> Score: ${data.films[0].rt_score}</p>`
 
