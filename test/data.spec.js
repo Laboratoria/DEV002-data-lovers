caf367d8b2e440d0f94f21f78669ac570f0f527b
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2022-12-11 20:56:11 -0500

    Añadí los tests de  filter y sortUpward

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,47 @@
-import { example, anotherExample } from '../src/data.js';
+import { filter, sortUpward } from '../src/data.js';
 
+const data1 = [{name:"bulbasaur", num:"001"},{name:"pikachu", num:"025"},{name:"charmander", num:"004"},{name:"charmeleon", num:"005"}];
+const data2 = [{name:"bulbasaur", num:"001"},{name:"pikachu", num:"025"},{name:"charmander", num:"004"},{name:"charmander", num:"004"}];
 
-describe('example', () => {
+describe('Buscar/filtrar el input ingresado...', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filter).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns `[{name:"charmander", num:"004"},{name:"charmeleon", num:"005"}]`', () => {
+    expect(filter(data1, "char")).toEqual([{name:"charmander", num:"004"},{name:"charmeleon", num:"005"}]);
+  });
+
+  it('returns `[{name:"pikachu", num:"025"}]`', () => {
+    expect(filter(data1, "PIKACHU")).toEqual([{name:"pikachu", num:"025"}]);
+  });
+
+  it('returns `[]`', () => {
+    expect(filter(data1, "t")).toEqual([]);
   });
 });
 
+describe('Ordenar el array de manera ascendente por name y number', ()=>{
+  it('is a function', ()=>{
+    expect(typeof sortUpward).toBe('function');
+  })
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+  it('returns `[{name:"bulbasaur", num:"001"},{name:"charmander", num:"004"},{name:"charmeleon", num:"005"},{name:"pikachu", num:"025"}]`', () => {
+    expect(sortUpward(data1, "name")).toEqual([{name:"bulbasaur", num:"001"},{name:"charmander", num:"004"},{name:"charmeleon", num:"005"},{name:"pikachu", num:"025"}]);
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns `[{name:"bulbasaur", num:"001"},{name:"charmander", num:"004"},{name:"charmander", num:"004"},{name:"pikachu", num:"025"}]`', () => {
+    expect(sortUpward(data2, "name")).toEqual([{name:"bulbasaur", num:"001"},{name:"charmander", num:"004"},{name:"charmander", num:"004"},{name:"pikachu", num:"025"}]);
   });
-});
+
+  it('returns `[{name:"bulbasaur", num:"001"},{name:"charmander", num:"004"},{name:"charmeleon", num:"005"},{name:"pikachu", num:"025"}]`', () => {
+    expect(sortUpward(data1, "number")).toEqual([{name:"bulbasaur", num:"001"},{name:"charmander", num:"004"},{name:"charmeleon", num:"005"},{name:"pikachu", num:"025"}]);
+  });
+
+  it('returns `[{name:"bulbasaur", num:"001"},{name:"charmander", num:"004"},{name:"charmander", num:"004"},{name:"pikachu", num:"025"}]`', () => {
+    expect(sortUpward(data2, "number")).toEqual([{name:"bulbasaur", num:"001"},{name:"charmander", num:"004"},{name:"charmander", num:"004"},{name:"pikachu", num:"025"}]);
+  });
+
+
+})
+
