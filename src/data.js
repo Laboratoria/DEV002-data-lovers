31134bd1cb783e6f68e65d46a3fe74f19d081b92
Mariana Arenas <arenasm8@hotmail.com>
2022-12-15 13:59:03 -0300

    funciona filter con búsqueda por director y título

--- src/data.js
+++ src/data.js
@@ -1,11 +1,10 @@
 // estas funciones son de ejemplo
 
 export const filtrarpeliculas = (dataGhibli,search) => {
-  let nuevaData=dataGhibli.filter(movie=>{
-     movie.title === search 
-  })
-  return nuevaData
-};
+  let nuevaData = dataGhibli.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase()) || movie.director.toLowerCase().includes(search.toLowerCase()))
+    return nuevaData
+  }
+
 //console.log(nuevaData)
 //export const prueba = (dataGhibli, cards) => {
  // return dataGhibli;
