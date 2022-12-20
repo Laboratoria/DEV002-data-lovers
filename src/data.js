bb9a3aff547d990582d02a6515c9532a766274ca
saamcruzzr <contacto.samcruz@gmail.com>
2022-12-20 09:24:24 -0600

    boton todas

--- src/data.js
+++ src/data.js
@@ -7,3 +7,15 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+//prueba de funcion filtrar
+// function filtrar(data) {
+//   for (let i = 2; data > i; i++) {
+//     if (data % i === 0) {
+//       return false;
+//     }
+//   }
+//   return data > 1;
+// }
+
+// console.log(array.filter(filtrar)); // [2, 3, 5, 7, 11, 13]
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
 const tarjetas = document.getElementById("tarjetas");
 data.films.forEach(element => {
     const tarjeta = `
@@ -17,6 +18,44 @@ data.films.forEach(element => {
     `
     tarjetas.innerHTML += tarjeta
 });
+
+if (todas) {
+  //botón todas
+  document.getElementById("todas").addEventListener("click",function() {
+  const tarjetas = document.getElementById("tarjetas");
+data.films.forEach(element => {
+    const tarjeta = `
+    <div class="contenedor"> 
+          <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
+          <section class="texto">
+            <p id= "titulo"> ${element.title}</p>
+            <p id= "datos"> Director: ${element.director} <br> Productor: ${element.producer} <br> Lanzamiento: ${element.release_date} <br> Score: ${element.rt_score}</p>
+          </section>
+    </div>
+    `
+    tarjetas.innerHTML += tarjeta});
+    console.log ("Mostrar Todas");
+  });
+} 
+//botón Hayao
+document.getElementById("hayao").addEventListener("click",function() { 
+  console.log ("Muestra Hayao M");
+  //enlazar función filter de data.js
+});
+
+document.getElementById("reciente").addEventListener("click",function() { 
+  console.log ("Muestra Más recientes")
+  //enlazar función filter de data.js
+});
+
+
+
+//botón hayao
+// document.getElementById("hayao").addEventListener("click",function() {
+//   let offset = document.getElementById("clave").value;
+//   let string = document.getElementById("input-original").value;
+//   document.getElementById("texto-cifrado").value = hayao.encode(offset, string);
+// });
 
 // const poster = document.getElementById("poster")
 // poster.innerHTML = ` <figure><img src="${data.films[0].poster}"</figure> `
